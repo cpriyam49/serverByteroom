9f36a9033bd12fa632fcf793e704bb9fb6cb1e74
Aleyaa <77834287+aleyaasinha@users.noreply.github.com>
2023-05-01 20:01:08 +0530

    Update Topic.js

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -1,11 +1,29 @@
 import React from "react";
 
-const Topic = ({ title, icon, link }) => {
-  const handleTopicClick = () => {
-    if (link) {
-      window.location.href = link;
-    }
-  };
+const handleEventClick = () => {
+  window.location.href = "https://eventbyteroom.netlify.app/";
+};
+
+const topics = [
+  //...
+  { title: "Events", icon: <BsMusicNoteBeamed />, onClick: handleEventClick },
+  //...
+];
+
+const Topic = ({ title, icon, onClick }) => {
+  return (
+    <div className="flex text-white/50 items-center text-[20px]">
+      <button
+        className="flex items-center hover:bg-[#2B2C4B] hover:text-white focus:bg-[#2B2C4B] focus:text-white w-full py-2.5 rounded-[0.4rem] font-semibold duration-100 ease-out"
+        onClick={onClick}
+      >
+        <div className="pl-4 text-[25px]">{icon}</div>
+        <p className="ml-2">{title}</p>
+      </button>
+    </div>
+  );
+};
+
 
   return (
     <div className="flex text-white/50 items-center text-[20px]   ">
