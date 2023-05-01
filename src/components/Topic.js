0baa111a3f9fb8cce5fc01fc29b701659a93a8bd
Aleyaa <77834287+aleyaasinha@users.noreply.github.com>
2023-05-01 19:52:08 +0530

    add event link option

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-const Topic = ({ title, icon }) => {
+const Topic = ({ title, icon, link }) => {
+  const handleClick = () => {
+    if (link) {
+      window.open(link, "_blank");
+    }
+  };
+
   return (
-    <div className="flex text-white/50 items-center text-[20px]   ">
-      <button className="flex items-center hover:bg-[#2B2C4B] hover:text-white focus:bg-[#2B2C4B] focus:text-white  w-full py-2.5 rounded-[0.4rem] font-semibold duration-100 ease-out ">
+    <div className="flex text-white/50 items-center text-[20px]">
+      <button
+        className="flex items-center hover:bg-[#2B2C4B] hover:text-white focus:bg-[#2B2C4B] focus:text-white  w-full py-2.5 rounded-[0.4rem] font-semibold duration-100 ease-out"
+        onClick={handleClick}
+      >
         <div className="pl-4 text-[25px]">{icon}</div>
         <p className="ml-2">{title}</p>
       </button>
@@ -12,4 +21,3 @@ const Topic = ({ title, icon }) => {
 };
 
 export default Topic;
-// #7289dc
