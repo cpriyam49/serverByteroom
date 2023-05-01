e7040e9689d8796ebc056d446ad03cf81afc3671
Aleyaa <77834287+aleyaasinha@users.noreply.github.com>
2023-05-01 20:10:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import CardPage from "./components/CardPage";
+import Discover from "./components/Discover";
 import Home from "./components/Home";
+import CardPage from "./components/CardPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/discover" element={<Discover />} />
         <Route path="/card" element={<CardPage />} />
       </Routes>
     </Router>
