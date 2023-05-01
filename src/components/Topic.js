606d8a5c55e4312420150f8e29dd2e3e32288f35
Aleyaa <77834287+aleyaasinha@users.noreply.github.com>
2023-05-01 19:57:14 +0530

    Update Topic.js

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 const Topic = ({ title, icon, link }) => {
-  const handleClick = () => {
+  const handleTopicClick = () => {
     if (link) {
-      window.open(link, "_blank");
+      window.location.href = link;
     }
   };
 
   return (
-    <div className="flex text-white/50 items-center text-[20px]">
+    <div className="flex text-white/50 items-center text-[20px]   ">
       <button
         className="flex items-center hover:bg-[#2B2C4B] hover:text-white focus:bg-[#2B2C4B] focus:text-white  w-full py-2.5 rounded-[0.4rem] font-semibold duration-100 ease-out"
-        onClick={handleClick}
+        onClick={handleTopicClick}
       >
         <div className="pl-4 text-[25px]">{icon}</div>
         <p className="ml-2">{title}</p>
